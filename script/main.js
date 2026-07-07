c275907647ad0244c287c76fd03d41d6ffc1b3ee
cos-13 <276010473+cos-13@users.noreply.github.com>
2026-07-07 23:08:23 +0900

    ヘッダーテスト

--- script/main.js
+++ script/main.js
@@ -1,13 +1,28 @@
-document.title="cos-13｜"+document.title;
+document.title="cos-13｜"+document.title;/*タイトル自動設定 */
 
-const he = document.getElementById("header");
+const he = document.getElementById("header");/*ヘッダーのelement */
 
-const pathdisp = document.createElement("a");
 
-const path = location.pathname.split("/");
-
+const path = location.pathname.split("/");//pathは配列
 console.log(path)
-pathdisp.setAttribute("href",location.href);
-pathdisp.textContent = location.pathname;
 
-he.appendChild(pathdisp);
+const home = document.createElement("a"); //ホームページだけ特別
+home.style.textDecoration = 'none';
+home.setAttribute("href",location.origin);
+home.textContent = "cos-13";
+he.appendChild(home);
+
+function dainari(){ //>を出力するだけのカス関数
+    const dai=document.createElement("a");
+    dai.textContent=">";
+    he.appendChild(dai);
+}
+
+for(const p of path){
+    if(p=="") continue;
+    dainari();
+    const f = document.createElement("a");
+    f.textContent=p;
+    f.setAttribute("href","index.html");
+    he.appendChild(f);
+} 
